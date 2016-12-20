bdc4776e88dbe9d0ea83e842d89fdfb977798224
Rashid Khan <github.fliplap@spamgourmet.com>
2016-12-20 14:22:21 -0700

    Make compatible with kibana master

--- index.js
+++ index.js
@@ -20,40 +20,6 @@ module.exports = function (kibana) {
         }
       },
       hacks: [],
-      modules: {
-        REflotCategories$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot.categories'),
-          imports: 'flot'
-        },
-        REflot$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot'),
-          imports: 'jquery'
-        },
-        REflotTime$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot.time'),
-          imports: 'flot'
-        },
-        REflotSymbol$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot.symbol'),
-          imports: 'flot'
-        },
-        REflotPie$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot.pie'),
-          imports: 'flot'
-        },
-        REflotCrosshair$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot.crosshair'),
-          imports: 'flot'
-        },
-        REflotSelection$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot.selection'),
-          imports: 'flot'
-        },
-        REflotStack$: {
-          path: path.resolve(__dirname, 'bower_components/Flot/jquery.flot.stack'),
-          imports: 'flot'
-        },
-      }
     },
 
     config: function (Joi) {
